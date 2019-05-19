799f4263464a2c4594ba2492ee8fe0c68e91014f
Samuel López Saura <samuellopezsaura@gmail.com>
2019-05-19 18:30:04 +0200

    Tasks to InstallDocker, start development setup (Local databases configuration) and finish development (power off containers) added

---
+++ tasks.py
@@ -0,0 +1,40 @@
+from invoke import task
+from invoke.exceptions import UnexpectedExit
+
+from config.settings.local import DATABASES
+
+
+db_container_name = 'dev_realitymismatch'
+
+
+@task
+def installdocker(c):
+    c.run('curl -fsSL https://get.docker.com -o /tmp/get-docker.sh')
+    c.run('sudo sh /tmp/get-docker.sh')
+
+
+@task
+def startdevelopment(c):
+
+    db_name = DATABASES['default']['NAME']
+    db_user = DATABASES['default']['USER']
+    db_pw = DATABASES['default']['PASSWORD']
+    container_tag = '11.3-alpine'
+    exposed_port = '5432'
+
+    print('Trying to raise database container')
+    try:
+        c.run(f'sudo docker start {db_container_name}')
+    except UnexpectedExit:
+        print('\033[1;31mContainer did\'nt exist before.\033[0m\n\
+Creating database container.')
+        c.run(
+            f'sudo docker run -d --name {db_container_name} -e POSTGRES_DB={db_name} -e POSTGRES_USER={db_user} -e POSTGRES_PASSWORD={db_pw} -p 127.0.0.1:{exposed_port}:5432 postgres:{container_tag}')
+
+
+@task
+def finishdevelopment(c):
+    print('Powering off database')
+    c.run(
+        f'sudo docker stop {db_container_name}'
+    )
