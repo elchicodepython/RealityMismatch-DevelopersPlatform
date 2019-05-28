90a64e6839cc1dd9c6b03348806395fdad8ae21a
Samuel López Saura <samuellopezsaura@gmail.com>
2019-05-28 23:53:37 +0200

    Update initial structure

---
+++ ui/src/app/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <header />
+        <div />
+        <footer />
+      </div>
+    );
+  }
+}
